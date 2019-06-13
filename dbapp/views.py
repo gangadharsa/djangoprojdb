f3dcfad793f137a95fb71154988fca7021649291
gangadharsa <49015288+gangadharsa@users.noreply.github.com>
2019-06-13 23:26:08 +0530

    Add files via upload

--- dbapp/views.py
+++ dbapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from django.http import HttpResponse
+from .serializers import ProductSerializer
+from rest_framework.renderers import JSONRenderer
 
 # Create your views here.
 def input(request):
@@ -17,3 +20,7 @@ def insert(request):
 def display(request):
     recs=Product.objects.all()
     return render(request,'display.html',{'records':recs})
+def productapi(request):
+    data=Product.objects.all()
+    serializer=ProductSerializer(data,many=True)
+    return HttpResponse(JSONRenderer().render(serializer.data))
